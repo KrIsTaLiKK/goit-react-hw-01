1e79675ebd6bc80a3a965700830731242bc198c9
Krystyna <zkristal95@gmail.com>
2024-01-18 12:10:34 -0300

    add default avatar and another statusIcon Online

--- src/components/friend-list/FriendListItem.jsx
+++ src/components/friend-list/FriendListItem.jsx
@@ -1,17 +1,40 @@
 import css from './FriendList.module.css';
 import { GiPlainCircle } from 'react-icons/gi';
+import { IoIosArrowDropdownCircle } from 'react-icons/io';
 
-export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
+export const FriendListItem = ({
+  friend: {
+    avatar = 'https://cdn-icons-png.flaticon.com/512/1998/1998597.png',
+    name,
+    isOnline,
+  },
+}) => {
   const statusClass = isOnline ? 'isOnline' : 'isOffline';
+  const statusText = isOnline ? 'Online' : 'Offline';
+  const statusIcon = isOnline ? (
+    <IoIosArrowDropdownCircle className={statusClass} />
+  ) : (
+    <GiPlainCircle className={statusClass} />
+  );
+
+  // const defaultAvatar =
+  //   'https://cdn-icons-png.flaticon.com/512/1998/1998597.png';
+  // const friendAvatar = avatar || defaultAvatar;
 
   return (
     <div className={css.itemInfo}>
-      <img className="avatar" src={avatar} alt={name} width="60" height="60" />
+      <img
+        className="avatar"
+        src={avatar}
+        alt={`Friend ${name}`}
+        width="60"
+        height="60"
+      />
       <div className={css.iconNameWrap}>
-        <GiPlainCircle className={statusClass} />
+        {statusIcon}
         <p className={css.friendName}>{name}</p>
       </div>
-      <p className={statusClass}>{isOnline ? 'Online' : 'Offline'}</p>
+      <p className={statusClass}>{statusText}</p>
     </div>
   );
 };
